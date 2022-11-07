91530e3b43b7bb681654945dbc88db8fea9c86f7
lesson17 <molatok@ya.ru>
2022-11-07 12:12:09 +0300

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+import telebot
+from telebot import types
+import parser
+
+
+bot = telebot.TeleBot('5607013372:AAGw8eGEUDl7-6vakh5xP9UG26OpYDuq9EU')
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра')
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == "привет":
+        bot.send_message(chat_id, 'Привет, я бот - парсер хабра.')
+    elif text == "как дела?":
+        bot.send_message(chat_id, 'Хорошо, а у тебя?')
+    else:
+        bot.send_message(chat_id, 'Простите, я вам не понял :(')
+
+bot.polling()
